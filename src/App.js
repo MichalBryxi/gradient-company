35d0b483562fdc734b2dab37f90195747d807bcd
Michal Bryxí <michal.bryxi@gmail.com>
2018-03-02 07:18:45 +0000

    Theming moved to React :)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+
 import Issue01 from './Issues/01-it-will-be-fun';
 import Issue02 from './Issues/02-coffee-addict';
 import Issue03 from './Issues/03-effectivity';
+
+import HomepageLayout from './Layout/Homepage';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Gradient company</h1>
-        <h2>Released issues</h2>
-          <Issue01 />
-          <Issue02 />
-        <h2>Drafts</h2>
-          <Issue03 />
-        <footer>
-          <p>Hosted on <a href="https://github.com/MichalBryxi/gradient-company">GitHub</a></p>
-          <p>Published on <a href="http://www.webtoons.com/en/challenge/gradient-company/list?title_no=155642">WebToon</a></p>
-        </footer>
+        <HomepageLayout>
+          <h1>Gradient company</h1>
+          <h2>Released issues</h2>
+            <Issue01 />
+            <Issue02 />
+          <h2>Drafts</h2>
+            <Issue03 />
+          <footer>
+            <p>Hosted on <a href="https://github.com/MichalBryxi/gradient-company">GitHub</a></p>
+            <p>Published on <a href="http://www.webtoons.com/en/challenge/gradient-company/list?title_no=155642">WebToon</a></p>
+          </footer>
+        </HomepageLayout>
       </div>
     );
   }
---
+++ src/Layout/Homepage.js
@@ -0,0 +1,180 @@
+import PropTypes from 'prop-types'
+import React, { Component } from 'react'
+import {
+  Button,
+  Container,
+  Divider,
+  Grid,
+  Header,
+  Icon,
+  Image,
+  List,
+  Menu,
+  Responsive,
+  Segment,
+  Sidebar,
+  Visibility,
+} from 'semantic-ui-react'
+
+/* eslint-disable react/no-multi-comp */
+/* Heads up! HomepageHeading uses inline styling, however it's not the best practice. Use CSS or styled components for
+ * such things.
+ */
+const HomepageHeading = ({ mobile }) => (
+  <Container text>
+    <Header
+      as='h1'
+      content='Imagine-a-Company'
+      inverted
+      style={{
+        fontSize: mobile ? '2em' : '4em',
+        fontWeight: 'normal',
+        marginBottom: 0,
+        marginTop: mobile ? '1.5em' : '3em',
+      }}
+    />
+    <Header
+      as='h2'
+      content='Do whatever you want when you want to.'
+      inverted
+      style={{
+        fontSize: mobile ? '1.5em' : '1.7em',
+        fontWeight: 'normal',
+        marginTop: mobile ? '0.5em' : '1.5em',
+      }}
+    />
+    <Button primary size='huge'>
+      Get Started
+      <Icon name='right arrow' />
+    </Button>
+  </Container>
+)
+
+HomepageHeading.propTypes = {
+  mobile: PropTypes.bool,
+}
+
+/* Heads up!
+ * Neither Semantic UI nor Semantic UI React offer a responsive navbar, however, it can be implemented easily.
+ * It can be more complicated, but you can create really flexible markup.
+ */
+class DesktopContainer extends Component {
+  state = {}
+
+  hideFixedMenu = () => this.setState({ fixed: false })
+  showFixedMenu = () => this.setState({ fixed: true })
+
+  render() {
+    const { children } = this.props
+    const { fixed } = this.state
+
+    return (
+      <Responsive {...Responsive.onlyComputer}>
+        <Visibility once={false} onBottomPassed={this.showFixedMenu} onBottomPassedReverse={this.hideFixedMenu}>
+          <Segment inverted textAlign='center' style={{ minHeight: 700, padding: '1em 0em' }} vertical>
+            <Menu
+              fixed={fixed ? 'top' : null}
+              inverted={!fixed}
+              pointing={!fixed}
+              secondary={!fixed}
+              size='large'
+            >
+              <Container>
+                <Menu.Item as='a' active>Home</Menu.Item>
+                <Menu.Item as='a'>Work</Menu.Item>
+                <Menu.Item as='a'>Company</Menu.Item>
+                <Menu.Item as='a'>Careers</Menu.Item>
+                <Menu.Item position='right'>
+                  <Button as='a' inverted={!fixed}>Log in</Button>
+                  <Button as='a' inverted={!fixed} primary={fixed} style={{ marginLeft: '0.5em' }}>Sign Up</Button>
+                </Menu.Item>
+              </Container>
+            </Menu>
+            <HomepageHeading />
+          </Segment>
+        </Visibility>
+
+        {children}
+      </Responsive>
+    )
+  }
+}
+
+DesktopContainer.propTypes = {
+  children: PropTypes.node,
+}
+
+class MobileContainer extends Component {
+  state = {}
+
+  handleToggle = () => this.setState({ sidebarOpened: !this.state.sidebarOpened })
+
+  render() {
+    const { children } = this.props
+    const { sidebarOpened } = this.state
+
+    return (
+      <Responsive {...Responsive.onlyMobile}>
+        <Sidebar.Pushable>
+          <Sidebar as={Menu} animation='uncover' inverted vertical visible={sidebarOpened}>
+            <Menu.Item as='a' active>Home</Menu.Item>
+            <Menu.Item as='a'>Work</Menu.Item>
+            <Menu.Item as='a'>Company</Menu.Item>
+            <Menu.Item as='a'>Careers</Menu.Item>
+            <Menu.Item as='a'>Log in</Menu.Item>
+            <Menu.Item as='a' primary>Sign Up</Menu.Item>
+          </Sidebar>
+
+          <Sidebar.Pusher dimmed={sidebarOpened} onClick={this.handleToggle} style={{ minHeight: '100vh' }}>
+            <Segment inverted textAlign='center' style={{ minHeight: 350, padding: '1em 0em' }} vertical>
+              <Container>
+                <Menu inverted pointing secondary size='large'>
+                  <Menu.Item onClick={this.handleToggle}>
+                    <Icon name='sidebar' />
+                  </Menu.Item>
+                  <Menu.Item position='right'>
+                    <Button as='a' inverted>Log in</Button>
+                    <Button as='a' inverted style={{ marginLeft: '0.5em' }}>Sign Up</Button>
+                  </Menu.Item>
+                </Menu>
+              </Container>
+              <HomepageHeading mobile />
+            </Segment>
+
+            {children}
+          </Sidebar.Pusher>
+        </Sidebar.Pushable>
+      </Responsive>
+    )
+  }
+}
+
+MobileContainer.propTypes = {
+  children: PropTypes.node,
+}
+
+const ResponsiveContainer = ({ children }) => (
+  <div>
+    <DesktopContainer>{children}</DesktopContainer>
+    <MobileContainer>{children}</MobileContainer>
+  </div>
+)
+
+ResponsiveContainer.propTypes = {
+  children: PropTypes.node,
+}
+
+const HomepageLayout = (props) => (
+  <ResponsiveContainer>
+    <Segment style={{ padding: '0em' }} vertical>
+      <Grid celled='internally' columns='equal' stackable>
+        <Grid.Row textAlign='center'>
+          <Grid.Column style={{ paddingBottom: '5em', paddingTop: '5em' }}>
+            {props.children}
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </Segment>
+  </ResponsiveContainer>
+)
+export default HomepageLayout
